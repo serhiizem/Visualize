c201d481115cc82809c87ecbca43ae6f57972376
RedSulfur <serhiizem@gmail.com>
2016-11-04 07:25:58 +0200

    All tests passed. MergedSortTest is missing

--- src/test/java/com/algorithms/InsertionSortTest.java
+++ src/test/java/com/algorithms/InsertionSortTest.java
@@ -1,26 +1,34 @@
 package com.algorithms;
 
 import com.algorithms.sorts.InsertionSort;
+import com.algorithms.util.Queue;
 import com.algorithms.util.SortRepresentation;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class InsertionSortTest {
 
-    private SortRepresentation sortRepresentation;
+    private Queue<SortRepresentation> sortRepresentationQueue;
+    private InsertionSort insertionSort;
 
     private static Integer[] VALID_INPUT = new Integer[] {80, 70, 60, 30, 20, 10};
     private static Integer[] CORRECT_RESULT = new Integer[] {10, 20, 30, 60, 70, 80};
 
     @Before
     public void setUp() throws Exception {
-        sortRepresentation = new SortRepresentation(VALID_INPUT);
+        sortRepresentationQueue = new Queue<>();
+        insertionSort = new InsertionSort(sortRepresentationQueue);
     }
 
     @Test
     public void shouldReturnSortedArrayOnValidInput() {
+        insertionSort.sort(VALID_INPUT);
 
+        Integer[] lastElementInAQueue = sortRepresentationQueue.getLast().getIntermediateResult();
+
+        assertThat(lastElementInAQueue, is(CORRECT_RESULT));
     }
 }
--- src/test/java/com/algorithms/SelectionSortTest.java
+++ src/test/java/com/algorithms/SelectionSortTest.java
@@ -1,26 +1,35 @@
 package com.algorithms;
 
 import com.algorithms.sorts.SelectionSort;
+import com.algorithms.util.Queue;
 import com.algorithms.util.SortRepresentation;
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class SelectionSortTest {
 
-    private SortRepresentation sortRepresentation;
+    private Queue<SortRepresentation> sortRepresentationQueue;
+    private SelectionSort selectionSort;
 
     private static Integer[] VALID_INPUT = new Integer[] {20, 30, 70, 10, 80, 60};
     private static Integer[] CORRECT_RESULT = new Integer[] {10, 20, 30, 60, 70, 80};
 
     @Before
     public void setUp() throws Exception {
-        sortRepresentation = new SortRepresentation(VALID_INPUT);
+        sortRepresentationQueue = new Queue<>();
+        selectionSort = new SelectionSort(sortRepresentationQueue);
     }
 
     @Test
-    public void shouldSortOnValidInput() {
+    public void lastElementPutInTheQueueShouldBeFullySorted() {
 
+        selectionSort.sort(VALID_INPUT);
+
+        Integer[] lastElementInTheQueue = sortRepresentationQueue.getLast().getIntermediateResult();
+
+        assertThat(lastElementInTheQueue, is(CORRECT_RESULT));
     }
 }
