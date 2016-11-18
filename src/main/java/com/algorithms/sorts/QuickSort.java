82746c5ab01013e8115b58dec1430aa78eca04de
RedSulfur <serhiizem@gmail.com>
2016-11-18 10:19:54 +0200

    Introduce comparator to QuickSort

--- src/main/java/com/algorithms/sorts/QuickSort.java
+++ src/main/java/com/algorithms/sorts/QuickSort.java
@@ -5,15 +5,20 @@ import com.algorithms.util.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Comparator;
+
 import static com.algorithms.sorts.Sorting.isLess;
 import static com.algorithms.sorts.Sorting.swap;
 
 @Component("quickSort")
 public class QuickSort extends Queueable implements Sorting {
 
+    private Comparator<Comparable> comparator; // in order to use QuickSort algorithm in generation classes
+
     @Autowired
     public QuickSort(Queue<SortRepresentation> sortRepresentationQueue) {
         super(sortRepresentationQueue);
+        comparator = (a, b) -> a.compareTo(b);
     }
 
     @Override
@@ -38,10 +43,10 @@ public class QuickSort extends Queueable implements Sorting {
         Comparable pivot = array[(left + right) / 2];
 
         while (i <= j) {
-            while (isLess(array[i], pivot)) {
+            while (comparator.compare(array[i], pivot) < 0) {
                 i++;
             }
-            while (isLess(pivot, array[j])) {
+            while (comparator.compare(pivot, array[j]) < 0) {
                 j--;
             }
             if(i <= j) {
